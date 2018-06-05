1da0ff77d21069a0df3f19964cf9c0ee839a65f8
wuyuanxin <792284322@qq.com>
2018-06-05 13:06:43 +0800

    update

---
+++ src/main/java/com/my/domain/User.java
@@ -0,0 +1,89 @@
+package com.my.domain;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Table(name = "user_test")
+public class User {
+    @Id
+    private Long id;
+
+    private String username;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    private Integer sex;
+
+    private Integer age;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return created_date
+     */
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    /**
+     * @param createdDate
+     */
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    /**
+     * @return sex
+     */
+    public Integer getSex() {
+        return sex;
+    }
+
+    /**
+     * @param sex
+     */
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * @return age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @param age
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/my/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.my.mapper;
+
+import com.my.domain.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
